4e5727b4cb03de36b375941c911dcb6fec34a7c0
oilrobot <oilrobot@outlook.com>
2024-10-18 10:38:19 +0800

    reqs_bat_install

---
+++ reqs.bat
@@ -0,0 +1,6 @@
+python -m pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu124
+python -m pip install -U scikit-learn
+python -m pip install matplotlib
+python -m pip install ipykernel
+python -m pip install pandas
+python -m pip freeze > reqs.txt
